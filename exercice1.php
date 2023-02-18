c6232a8671da0bebda74b6c58f2f558fdb4b6e5b
yanbert <yanbert@gmail.com>
2023-02-18 17:44:48 +0000

    mes commentaires dans le code
    /*
    tu utilises du code python pour te connecter en php???
    
    le lien vers la page php pour postgres
    
    https://www.php.net/manual/en/book.pgsql.php
    */

--- exercice1.php
+++ exercice1.php
@@ -1,10 +1,18 @@
 <?php 
 
+/*
+tu utilises du code python pour te connecter en php???
+
+le lien vers la page php pour postgres
+
+https://www.php.net/manual/en/book.pgsql.php
+*/
 conn = psycopg2.connect(
     host="localhost",
     database="nom_de_la_base_de_donnees",
     user="nom_utilisateur",
     password="mot_de_passe")
+
 
 print("Opened database successfully")
 ?>
